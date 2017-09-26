75f1fa641f9c7a46364a0b7175be94135fdf7ac8
Rebecca Lynde-Scott <MsMadGirl@users.noreply.github.com>
2017-09-25 19:17:27 -0700

    Upload Hello World and Salad
    Just stuff from the first class

---
+++ HelloJava/src/Hello.java
@@ -0,0 +1,15 @@
+
+public class Hello {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("Hello world!");
+		/* backslash is escape sequence, // or /* for comments*/
+		System.out.println("C:\nin\the downward spiral"); //ouch
+		System.out.println("/ \\ // \\\\ /// \\\\\\");
+		System.out.println("\ta\tb\tc");
+		
+		
+	}
+
+}
---
+++ Salad/src/SaladAlgorithm.java
@@ -0,0 +1,15 @@
+
+public class SaladAlgorithm {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("Tear lettuce");
+		System.out.println("Chop tomatoes");
+		System.out.println("Shake dressing");
+		System.out.println("Put lettuce and tomato in bowl");
+		System.out.println("Add dressing");
+		System.out.println("Toss together");
+		
+	}
+
+}
